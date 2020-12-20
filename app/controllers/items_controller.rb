8784c86fd35f10725aded0b46df6eb6a149d2c56
toshiyuki43 <shi6to3rd@gmail.com>
2020-12-20 16:02:32 +0900

    商品出品機能　テスト修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,8 +11,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-      if @item.valid?
-        @item.save
+      if @item.save
         redirect_to root_path
       else
         render :new
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     title { 'sample' }
     text { 'sample' }
-    price { '10000' }
-    category_id { '2' }
-    item_condition_id { '2' }
-    shipping_charge_id { '2' }
-    prefecture_id { '2' }
-    shipping_day_id { '2' }
+    price { 10000 }
+    category_id { 2 }
+    item_condition_id { 2 }
+    shipping_charge_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
     association :user
 
     after(:build) do |item|
