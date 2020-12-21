8fc79e3af451107f651b5fb1256a7ec88e5eee5a
toshiyuki43 <shi6to3rd@gmail.com>
2020-12-21 20:43:49 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    redirect_to action: :index unless current_user.id == @item.user.id && @item.order != nil
   end
 
   def update
